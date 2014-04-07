93de786159678ceba8d342f02b2b7ce40d67b097
Kevin P. Dyer <kpdyer@gmail.com>
2014-04-07 10:28:38 -0700

    more verbose comments

--- tests/gnulinux-i386/install_fteproxy.sh
+++ tests/gnulinux-i386/install_fteproxy.sh
@@ -15,19 +15,22 @@
 # You should have received a copy of the GNU General Public License
 # along with fteproxy.  If not, see <http://www.gnu.org/licenses/>.
 
-# install obfsproxy
+# install tor repo
 echo "deb http://deb.torproject.org/torproject.org wheezy main" >> /etc/apt/sources.list
 echo "deb http://deb.torproject.org/torproject.org tor-experimental-0.2.5.x-wheezy main" >> /etc/apt/sources.list
 gpg --keyserver keys.gnupg.net --recv 886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
-apt-get -y update
-#apt-get -y install tor deb.torproject.org-keyring
-apt-get -y install tor obfsproxy
 
-# install fteproxy
-echo "deb http://fteproxy.org/deb wheezy main" >> /etc/apt/sources.list
+# install fteproxy repo
+echo "deb http://fteproxy.org/deb/ stable/" >> /etc/apt/sources.list
 gpg --keyserver keys.gnupg.net --recv 6B898EE18FBA6390
 gpg --export 6B898EE18FBA6390 | sudo apt-key add -
+
+# do update
 apt-get -y update
+
+# install fteproxy
 apt-get -y install fteproxy
+
+# test fteproxy
 fteproxy --mode test
