fab734ef47e93a3934f77d35c8f865e71f26e3c1
Kevin P. Dyer <kpdyer@gmail.com>
2014-04-10 22:13:01 -0700

    wheezy -> precise

--- tests/ubuntu-12.04-i386/install_fteproxy.sh
+++ tests/ubuntu-12.04-i386/install_fteproxy.sh
@@ -16,8 +16,8 @@
 # along with fteproxy.  If not, see <http://www.gnu.org/licenses/>.
 
 # install tor repo
-echo "deb http://deb.torproject.org/torproject.org wheezy main" >> /etc/apt/sources.list
-echo "deb http://deb.torproject.org/torproject.org tor-experimental-0.2.5.x-wheezy main" >> /etc/apt/sources.list
+echo "deb http://deb.torproject.org/torproject.org precise main" >> /etc/apt/sources.list
+echo "deb http://deb.torproject.org/torproject.org tor-experimental-0.2.5.x-precise main" >> /etc/apt/sources.list
 gpg --keyserver keys.gnupg.net --recv 886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 
